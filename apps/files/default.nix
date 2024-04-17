02d8c5d7f29174fe130a327b5ab288902acf28cb
Rev <coding.refusing492@passmail.net>
2024-04-17 20:16:16 +0700

    Use thunar instead

--- apps/files/default.nix
+++ apps/files/default.nix
@@ -2,13 +2,18 @@
 
 {
   environment.systemPackages = with pkgs; [
-    nemo
     unzip
     zip
     unrar
     rar
     p7zip
+    xarchiver
   ];
 
   services.gvfs.enable = true;
+
+  programs.thunar = {
+    enable = true;
+    plugins = with pkgs.xfce; [ thunar-archive-plugin thunar-volman ];
+  };
 }
