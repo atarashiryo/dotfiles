2c1a9f0bca111629f4740c6b3fbc058ed5d939be
Rev <coding.refusing492@passmail.net>
2024-04-17 20:17:13 +0700

    Some update

--- configuration.nix
+++ configuration.nix
@@ -94,9 +94,7 @@
   users.users.rev = {
     isNormalUser = true;
     description = "Rev";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-    ];
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
   };
 
   # Enable unfree software
@@ -125,7 +123,7 @@
 
   # Home Manager State Version
   home-manager.users.rev = { pkgs, ... }: {
-    home.stateVersion = "23.11";
+    home.stateVersion = "24.05";
   };
 
   # Delete generation older than 7 days
