ea59cdc342a4b3166121819d01af96b20998beed
Rev <coding.refusing492@passmail.net>
2024-04-29 21:26:04 +0700

    Liked nemo more

--- apps/files/default.nix
+++ apps/files/default.nix
@@ -2,6 +2,7 @@
 
 {
   environment.systemPackages = with pkgs; [
+    cinnamon.nemo
     unzip
     zip
     unrar
@@ -11,9 +12,4 @@
   ];
 
   services.gvfs.enable = true;
-
-  programs.thunar = {
-    enable = true;
-    plugins = with pkgs.xfce; [ thunar-archive-plugin thunar-volman ];
-  };
 }
